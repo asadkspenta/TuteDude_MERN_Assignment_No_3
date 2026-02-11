f8674d206f96c53eaf469299d64e0d8f0cefb6f6
asadkspenta <asadk.spenta@gmail.com>
2026-02-11 17:21:35 +0530

    Initial commit - MERN Assignment 3

---
+++ main.js
@@ -0,0 +1,27 @@
+
+function nameData() {
+    let name = document.getElementById("txtName").value;
+    document.getElementById("headerText").innerText = "Hello " + name;
+}
+
+let cards = document.querySelectorAll(".card");
+
+cards.forEach((card) =>{
+
+    card.addEventListener("click", function() {
+
+        if (card.classList.contains("cardOne")) {
+            card.classList.toggle("red");
+        }
+        else if (card.classList.contains("cardTwo")) {
+            card.classList.toggle("blue");
+        }
+        else if (card.classList.contains("cardThree")) {
+            card.classList.toggle("green");
+        }
+        else if (card.classList.contains("cardFour")) {
+            card.classList.toggle("orange");
+        }
+
+    });
+});
